43fbfe9b8b97953510f56336104f485f06e29918
nellerr <nellerr@mail.ru>
2020-02-26 12:46:15 +0200

    message

---
+++ src/Rate/Rate.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Rate.css';
+
+import Calc from '../Calc/Calc';
+
+class Rate extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      'date': "",
+      'currencyRate': {
+
+      }
+      
+    }
+    this.getRate();
+    this.currency = ['USD', 'RUB', 'CAD', 'PHP'];
+  }
+  
+  getRate = () =>{
+    fetch ('https://api.exchangeratesapi.io/latest')
+    .then(
+      data =>{
+        return data.json()
+      }
+    )
+    .then (data =>{
+      console.log(data);
+      this.setState({date: data.date});
+      let result = {};
+      for(let i=0;i<this.currency.length;i++){
+        result[this.currency[i]] = data.rates[this.currency[i]]
+      }
+      this.setState({currencyRate: result})
+    })
+  }
+  render(){
+  return (
+    <div className="Rate">
+    <h3> Курс валют на {this.state.date}</h3>
+    <div className="flex-container">
+      {Object.keys(this.state.currencyRate).map((keyName,i)=>
+      (
+        <div className="block flex-item" key={keyName}>
+        <div className="currency-name">{keyName}</div>
+        <div className="currency-in">{this.state.currencyRate[keyName].toFixed(2)}</div>
+       <p>  * Можно купить за 1 EUR </p>
+    </div>
+
+      )
+
+      )}
+      
+    </div>
+    <Calc rate={this.state.currencyRate} />
+    </div>
+
+  );
+}
+}
+export default Rate;
---
+++ src/exchange/Exchange.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './exchange.css';
+
+class About extends React.Component {
+ 
+  
+  render(){
+  return (
+   
+   
+<div>Пункты обмена</div>
+
+
+  );
+}
+}
+export default About;
